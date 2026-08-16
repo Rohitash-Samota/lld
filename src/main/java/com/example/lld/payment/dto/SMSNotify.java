69a0161db38d82a435e4d7b97c15410dc908825d
Rohitash Samota <rohitsamota44@gmail.com>
2026-08-16 19:49:16 +0530

    SMS Notify

--- src/main/java/com/example/lld/payment/dto/SMSNotify.java
+++ src/main/java/com/example/lld/payment/dto/SMSNotify.java
@@ -1,6 +1,23 @@
 package com.example.lld.payment.dto;
 
-public class SMSNotify{
+public class SMSNotify {
     private String smsId;
     private String content;
+
+    public String getSmsId() {
+        return smsId;
+    }
+
+    public void setSmsId(String smsId) {
+        this.smsId = smsId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
 }
